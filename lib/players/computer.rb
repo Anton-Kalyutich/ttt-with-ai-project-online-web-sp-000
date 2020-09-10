16c991fc88799565ada340d7fd31d71bb02aedcf
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-09-10 23:47:27 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,6 +3,7 @@ module Players
     
     def move(board)
       return "5" if !board.taken?("5")
+      
       return "1" if !board.taken?("1")
       return "3" if !board.taken?("3")
       return "7" if !board.taken?("7")
